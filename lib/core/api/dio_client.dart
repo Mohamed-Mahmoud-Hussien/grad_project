ff19176b2dce610c81b468994ee7e7737bb21e06
Mohamed-Mahmoud-Hussien <mohamedmahmoud9988840@gmail.com>
2026-06-27 03:30:29 +0300

    Restore firebase_options.dart

--- lib/core/api/dio_client.dart
+++ lib/core/api/dio_client.dart
@@ -1,11 +1,29 @@
 import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'api_constants.dart';
 
 class DioClient {
   static final Dio dio = Dio(
     BaseOptions(
       baseUrl: ApiConstants.baseUrl,
-      headers: {"Content-Type": "application/json"},
+      headers: {
+        "Content-Type": "application/json",
+      },
     ),
-  );
+  )..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final prefs = await SharedPreferences.getInstance();
+
+          final token = prefs.getString("accessToken");
+
+          if (token != null && token.isNotEmpty) {
+            options.headers["Authorization"] = "Bearer $token";
+          }
+
+          return handler.next(options);
+        },
+      ),
+    );
 }
